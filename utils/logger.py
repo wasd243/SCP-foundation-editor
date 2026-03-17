398162dd71081744bfa22d6a79da7926fa50decf
Zichen Wang <wzc_2205@163.com>
2026-03-18 04:53:55 +0800

    feat: Add utility modules for logging and banner display.

--- utils/logger.py
+++ utils/logger.py
@@ -13,7 +13,7 @@ import datetime
 from formats.wikidot.wikidot_parser import HAS_FTML
 
 
-VERSION = "v2.0.1-beta.3"
+VERSION = "v2.0.1-beta.4"
 ENGINE = "FTML + PyQt6"
 EDITOR = "SCP Wiki WYSIWYG Editor"
 
