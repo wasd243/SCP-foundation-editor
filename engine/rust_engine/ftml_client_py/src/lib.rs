28e63a28929290a46630e87ce733e8f155a1a4b2
wasd243 <CWwithoutfish@outlook.com>
2026-05-03 20:23:02 -0400

    refactor(AIM)! from python to rust, bridge: pyO3

--- engine/rust_engine/ftml_client_py/src/lib.rs
+++ engine/rust_engine/ftml_client_py/src/lib.rs
@@ -11,8 +11,14 @@ fn process_acs(py: Python<'_>, text: &str, store: &PyAny) -> PyResult<String> {
     ACS::process_acs(py, text, store)
 }
 
+#[pyfunction]
+fn process_aim(py: Python<'_>, text: &str, store: &PyAny) -> PyResult<String> {
+    AIM::process_aim(py, text, store)
+}
+
 #[pymodule]
 fn ftml_client_py(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(process_acs, m)?)?;
+    m.add_function(wrap_pyfunction!(process_aim, m)?)?;
     Ok(())
 }
