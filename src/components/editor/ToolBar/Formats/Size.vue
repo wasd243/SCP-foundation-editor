969e533b125a84ad89574ad3959957cb2841da3e
wasd243 <CWwithoutfish@outlook.com>
2026-05-08 19:54:26 -0400

    feat(UI): add size select bar

--- src/components/editor/ToolBar/Formats/Size.vue
+++ src/components/editor/ToolBar/Formats/Size.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Larger from "./Size/Larger.vue";
 import Smaller from "./Size/Smaller.vue";
+import List from "./Size/List.vue";
 </script>
 
 <template>
   <Larger/>
   <Smaller/>
+  <List/>
 </template>
---
+++ src/components/editor/ToolBar/Formats/Size/List.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const isOpen = ref(false);
+const fontSizes = [12, 14];
+const selectedSize = ref(fontSizes[0]);
+
+function toggleList() {
+  isOpen.value = !isOpen.value;
+}
+
+function selectSize(size: number) {
+  selectedSize.value = size;
+  isOpen.value = false;
+}
+</script>
+
+<template>
+  <div class="format-size-list">
+    <button
+      class="format-size-button list"
+      type="button"
+      :aria-expanded="isOpen"
+      @click="toggleList"
+    >
+      <span>{{ selectedSize }}</span>
+      <span class="format-size-button-arrow">⌄</span>
+    </button>
+
+    <div v-if="isOpen" class="format-size-menu">
+      <button
+        v-for="size in fontSizes"
+        :key="size"
+        class="format-size-menu-item"
+        type="button"
+        @click="selectSize(size)"
+      >
+        {{ size }}
+      </button>
+    </div>
+  </div>
+</template>
