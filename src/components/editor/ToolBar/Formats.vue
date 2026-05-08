6efcb99f3b7973745c6c8a03cec4def025996fd1
wasd243 <CWwithoutfish@outlook.com>
2026-05-08 15:38:02 -0400

    chore: check point commit

---
+++ src/components/editor/ToolBar/Formats.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "Formats"
+})
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/editor/TopBar/Insert.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+name: "Insert"
+})
+</script>
+
+<template>
+  $END$
+</template>
+
+<style scoped>
+
+</style>
