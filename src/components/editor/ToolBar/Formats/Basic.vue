292c14825c80bd0c95521905c8852537a7505f3f
wasd243 <CWwithoutfish@outlook.com>
2026-05-08 18:57:24 -0400

    feat(UI): add sub and sup for ribbon layout

--- src/components/editor/ToolBar/Formats/Basic.vue
+++ src/components/editor/ToolBar/Formats/Basic.vue
@@ -3,6 +3,8 @@ import Bold from "./Basic/Bold.vue";
 import Italic from "./Basic/Italic.vue";
 import Underline from "./Basic/Underline.vue";
 import Strikethrough from "./Basic/Strikethrough.vue";
+import Sub from "./Basic/Sub.vue";
+import Sup from "./Basic/Sup.vue";
 </script>
 
 <template>
@@ -10,4 +12,6 @@ import Strikethrough from "./Basic/Strikethrough.vue";
   <Italic/>
   <Underline/>
   <Strikethrough/>
+  <Sub/>
+  <Sup/>
 </template>
---
+++ src/components/editor/ToolBar/Formats/Basic/Sup.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <button class="format-basic-button sup-s">X²</button>
+</template>
