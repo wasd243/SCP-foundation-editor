977d3ed694bf3aaaa9d575e13820f7ffb5521f80
wasd243 <CWwithoutfish@outlook.com>
2026-05-09 08:57:44 -0400

    feat(UI): add ribbon separator

--- src/components/editor/TopBar/Ribbon/HomePage.vue
+++ src/components/editor/TopBar/Ribbon/HomePage.vue
@@ -6,6 +6,8 @@ import Formats from "../../ToolBar/Formats.vue";
     <Formats/>
     <div class="ribbon-separator"></div>
     <div class="ribbon-separator small-1"></div>
+    <div class="ribbon-separator small-2"></div>
     <div class="ribbon-separator normal-1"></div>
+    <div class="ribbon-separator normal-2"></div>
   </div>
 </template>
