0aa02e830de8db613ed9ddbea2f66f0dfe53926f
wasd243 <CWwithoutfish@outlook.com>
2026-05-09 13:33:11 -0400

    feat(parser): add parser

---
+++ crates/Wparser/src/lib.rs
@@ -0,0 +1,48 @@
+use std::borrow::Cow;
+
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct FtmlParseOutput {
+    pub html: String,
+    pub ast_json: String,
+}
+
+pub fn render_wikidot_to_html_and_ast(source_text: &str) -> Result<FtmlParseOutput, String> {
+    let settings = ftml::settings::WikitextSettings::from_mode(
+        ftml::settings::WikitextMode::Page,
+        ftml::layout::Layout::Wikidot,
+    );
+
+    let page_info = ftml::data::PageInfo {
+        page: Cow::Borrowed("page"),
+        category: None,
+        site: Cow::Borrowed("scp-wiki"),
+        title: Cow::Borrowed("Editor"),
+        alt_title: None,
+        score: ftml::data::ScoreValue::Integer(0),
+        tags: vec![],
+        language: Cow::Borrowed("cn"),
+    };
+
+    let mut wikitext = source_text.to_string();
+    ftml::preprocess(&mut wikitext);
+
+    let tokens = ftml::tokenize(&wikitext);
+    let parsed_result = ftml::parse(&tokens, &page_info, &settings);
+
+    let (tree, _warnings) = parsed_result.into();
+
+    let ast_json = serde_json::to_string_pretty(&tree)
+        .map_err(|err| err.to_string())?;
+
+    use ftml::render::Render;
+
+    let renderer = ftml::render::html::HtmlRender;
+    let html_output = renderer.render(&tree, &page_info, &settings);
+
+    Ok(FtmlParseOutput {
+        html: html_output.body.to_string(),
+        ast_json,
+    })
+}
