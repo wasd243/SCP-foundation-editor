834363e054547699d2778716c1b3a73b9bd4980a
wasd243 <CWwithoutfish@outlook.com>
2026-05-09 16:58:28 -0400

    chore(UI): add table context menu components place-holder files

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import TopBar from "./components/editor/TopBar.vue";
 import "./styles/global.css";
 import EditorCanvas from "./components/editor/EditorCanvas.vue";
+import ContextMenu from "./components/editor/ContextMenu.vue";
 </script>
 
 <template>
   <TopBar/>
+  <ContextMenu/>
   <EditorCanvas/>
 </template>
 
---
+++ src/components/editor/ContextMenu/TableContextMenu/AddColumn.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "AddColumn"
+})
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
