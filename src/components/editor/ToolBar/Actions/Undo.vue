463c2b99df9e4776dbca1cd644d1120e5a0e508a
wasd243 <CWwithoutfish@outlook.com>
2026-05-09 09:55:06 -0400

    chore(UI): add place-holder files for actions and toolbar components

---
+++ src/components/editor/ToolBar/Actions/Undo.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import {defineComponent} from 'vue'
+
+export default defineComponent({
+  name: "Undo"
+})
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
