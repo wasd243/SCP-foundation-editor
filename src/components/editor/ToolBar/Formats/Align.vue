84058ba518f5bc882e71aeaef814eafacbc25e09
wasd243 <CWwithoutfish@outlook.com>
2026-05-09 08:49:12 -0400

    feat(UI): add align button `→` to toolbar

--- src/components/editor/ToolBar/Formats/Align.vue
+++ src/components/editor/ToolBar/Formats/Align.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import Left from "./Align/Left.vue";
+import Right from "./Align/Right.vue";
 </script>
 
 <template>
   <Left/>
+  <Right/>
 </template>
--- src/components/editor/ToolBar/Formats/Align/Right.vue
+++ src/components/editor/ToolBar/Formats/Align/Right.vue
@@ -1,15 +1,5 @@
-<script lang="ts">
-import {defineComponent} from 'vue'
-
-export default defineComponent({
-  name: "Right"
-})
-</script>
+<script setup lang="ts"></script>
 
 <template>
-
+  <button class="format-basic-button align-right">→</button>
 </template>
-
-<style scoped>
-
-</style>
