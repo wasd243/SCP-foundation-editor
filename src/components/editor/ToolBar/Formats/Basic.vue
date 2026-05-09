3511b90b88f069803d654118b6568a3d1cb8c88b
wasd243 <CWwithoutfish@outlook.com>
2026-05-09 08:03:04 -0400

    feat(UI): add quote button to basic toolbar

--- src/components/editor/ToolBar/Formats/Basic.vue
+++ src/components/editor/ToolBar/Formats/Basic.vue
@@ -5,6 +5,7 @@ import Underline from "./Basic/Underline.vue";
 import Strikethrough from "./Basic/Strikethrough.vue";
 import Sub from "./Basic/Sub.vue";
 import Sup from "./Basic/Sup.vue";
+import Quote from "./Basic/Quote.vue";
 </script>
 
 <template>
@@ -14,4 +15,5 @@ import Sup from "./Basic/Sup.vue";
   <Strikethrough/>
   <Sub/>
   <Sup/>
+  <Quote/>
 </template>
