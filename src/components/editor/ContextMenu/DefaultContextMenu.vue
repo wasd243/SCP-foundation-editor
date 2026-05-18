4e2313d659bb609270a5fc4c41d32b9b95a4d485
wasd243 <CWwithoutfish@outlook.com>
2026-05-18 19:54:06 -0400

    feat(UI): add insert footnote in context menu

--- src/components/editor/ContextMenu/DefaultContextMenu.vue
+++ src/components/editor/ContextMenu/DefaultContextMenu.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Copy from "./DefaultContextMenu/Copy.vue";
+import InsertFootnote from "./DefaultContextMenu/InsertFootnote.vue";
 import Paste from "./DefaultContextMenu/Paste.vue";
 </script>
 
@@ -7,5 +8,7 @@ import Paste from "./DefaultContextMenu/Paste.vue";
   <div class="editor-context-menu">
     <Copy/>
     <Paste/>
+    <div class="context-menu-separator"></div>
+    <InsertFootnote/>
   </div>
 </template>
---
+++ src/components/editor/ContextMenu/DefaultContextMenu/InsertFootnote.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { insertEditorFootnote } from "../../../../stores/btnToolBar/btnNonIncludeComponents/btnFootnote.ts";
+</script>
+
+<template>
+  <button
+      class="context-menu-item"
+      type="button"
+      @click="insertEditorFootnote"
+  >
+    <span>Footnote</span>
+  </button>
+</template>
