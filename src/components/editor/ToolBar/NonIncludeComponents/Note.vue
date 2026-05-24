3008ae473e07ce89184eb3e74618193e3b917427
wasd243 <CWwithoutfish@outlook.com>
2026-05-24 11:01:37 -0400

    feat(note): add note insertion
    feat(note): add Note icon
    
    feat(note): add note insertion

--- src/components/editor/ToolBar/NonIncludeComponents/Note.vue
+++ src/components/editor/ToolBar/NonIncludeComponents/Note.vue
@@ -1,5 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Note from "../../../../assets/icons/Note.svg";
+import { insertEditorNote } from "../../../../stores/btnToolBar/btnNonIncludeComponents/btnNote.ts";
+</script>
 
 <template>
-  <button class="component-card">Note</button>
+  <button class="component-card" type="button" @click="insertEditorNote">
+    <img :src="Note" alt="Note"/>
+    Note
+  </button>
 </template>
