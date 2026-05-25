b8498359a8b79ab4f9e968a2073be0f33e3dd441
wasd243 <CWwithoutfish@outlook.com>
2026-05-25 16:58:32 -0400

    chore: add insertion place holders

---
+++ src/components/editor/ToolBar/Insertion/InsertImage.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import InsertImageIcon from "../../../../../assets/icons/InsertImage.svg";
+</script>
+
+<template>
+
+</template>
--- src/components/editor/TopBar/Ribbon/InsertPage.vue
+++ src/components/editor/TopBar/Ribbon/InsertPage.vue
@@ -1,8 +1,11 @@
 <!--InsertPage.vue is used to display the components-->
 
 <script setup lang="ts">
+
 </script>
 
 <template>
-  <div class="ribbon-layout" data-ribbon-page="insert"></div>
+  <div class="ribbon-layout" data-ribbon-page="insert">
+
+  </div>
 </template>
