c12e0e51a5d237d94b38bb92435042e0cb10651d
wasd243 <CWwithoutfish@outlook.com>
2026-05-28 12:24:35 -0400

    feat(user): add user parser for editor html
    add placeholders
    
    add interceptor for`[[user]]` `[[*user ]]`
    
    add user external parser functions
    
    add user style support

---
+++ crates/w_parser/src/ftml_interceptor/user/mod.rs
@@ -0,0 +1,3 @@
+pub mod user_interceptor;
+pub mod user_cleaner;
+pub mod user_parser;
---
+++ crates/w_parser/src/ftml_interceptor/user/user_interceptor.rs
@@ -0,0 +1,15 @@
+use regex::Regex;
+
+pub fn user_interceptor(text: &str) -> String {
+    let re = Regex::new(r"(?i)\[\[user\s+(.+?)\]\]").unwrap();
+
+    re.replace_all(text, "~_WJ_USER_PARSER_BEGIN_~$1~_WJ_USER_PARSER_END_~")
+        .into_owned()
+}
+
+pub fn user_with_img_interceptor(text: &str) -> String {
+    let re = Regex::new(r"(?i)\[\[\*user\s+(.+?)\]\]").unwrap();
+
+    re.replace_all(text, "~_WJ_USER_WITH_IMG_PARSER_BEGIN_~$1~_WJ_USER_WITH_IMG_PARSER_END_~")
+        .into_owned()
+}
