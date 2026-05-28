1a5de5f442f570909e24e5871a4a8ad5f7d887c1
wasd243 <CWwithoutfish@outlook.com>
2026-05-28 15:11:20 -0400

    feat(user): add `[[user]]` insertion button
    add placeholder
    
    update button to include user
    
    update `[[*user` insertion

--- src/components/editor/ToolBar/NonIncludeComponents/User.vue
+++ src/components/editor/ToolBar/NonIncludeComponents/User.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import User from "../../../../assets/icons/User.svg"
+import User from "../../../../assets/icons/User.svg";
+import { promptInsertEditorUser } from "../../../../stores/btnToolBar/btnNonIncludeComponents/btnUser.ts";
 </script>
 
 <template>
-  <button class="component-card">
+  <button class="component-card" type="button" @click="promptInsertEditorUser">
     <img :src="User" alt="User"/>
     User
   </button>
