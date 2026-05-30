827184715dad7a4bc5f4a0cb507a1d92e961d5b2
wasd243 <CWwithoutfish@outlook.com>
2026-05-30 11:48:37 -0400

    test: add export_default_json test

--- crates/ltmf/src/lib.rs
+++ crates/ltmf/src/lib.rs
@@ -10,32 +10,13 @@ pub fn export_wikitext(json: &str) -> Result<String, String> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use crate::import_json::import_json;
-    use serde_json::Value;
     use std::fs;
 
     #[test]
-    fn test_import_json() {
-        let mut json = String::new();
-
-        import_json(&mut json);
-
-        json = export_wikitext(&json).unwrap();
-
-        println!("{json}");
-    }
-
-    #[test]
-    fn test_sanitize_8() {
+    fn test_export_default_json() {
         let json = export_wikitext("").unwrap();
-        let expected_json =
-            fs::read_to_string(concat!(env!("CARGO_MANIFEST_DIR"), "/test/json/sanitize_11.json"))
-                .unwrap();
+        let output_path = concat!(env!("CARGO_MANIFEST_DIR"), "/test/json/default.json");
 
-        let actual: Value = serde_json::from_str(&json).unwrap();
-        let expected: Value = serde_json::from_str(&expected_json).unwrap();
-
-        assert_eq!(actual, expected);
-        println!("Pass");
+        fs::write(output_path, json).unwrap();
     }
 }
