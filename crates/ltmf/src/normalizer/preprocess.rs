6745e12f654f9db227746af0fcb7e9b9d14a1229
wasd243 <CWwithoutfish@outlook.com>
2026-05-30 09:57:04 -0400

    feat(sanitizer): add sanitize_text_align module

--- crates/ltmf/src/normalizer/preprocess.rs
+++ crates/ltmf/src/normalizer/preprocess.rs
@@ -3,7 +3,8 @@ use serde_json::Value;
 
 use crate::normalizer::preprocess::sanitize::{
     sanitize_data_editor::sanitize_data_editor, sanitize_empty_attrs::sanitize_empty_attrs,
-    sanitize_null::sanitize_null, sanitize_wj_inline_tag::sanitize_wj_inline_tag,
+    sanitize_null::sanitize_null, sanitize_text_align::sanitize_text_align,
+    sanitize_wj_inline_tag::sanitize_wj_inline_tag,
 };
 
 pub mod sanitize;
@@ -17,6 +18,7 @@ pub fn preprocess(json: &str) -> Result<String, String> {
     let sanitized_json = sanitize_null(&json_value);
     let sanitized_json = sanitize_data_editor(sanitized_json);
     let sanitized_json = sanitize_wj_inline_tag(sanitized_json);
+    let sanitized_json = sanitize_text_align(sanitized_json);
     let sanitized_json = sanitize_empty_attrs(&sanitized_json);
 
     let json = serde_json::to_string_pretty(&sanitized_json).map_err(|error| error.to_string())?;
--- crates/ltmf/src/normalizer/preprocess/sanitize.rs
+++ crates/ltmf/src/normalizer/preprocess/sanitize.rs
@@ -1,4 +1,5 @@
 pub mod sanitize_data_editor;
 pub mod sanitize_empty_attrs;
 pub mod sanitize_null;
+pub mod sanitize_text_align;
 pub mod sanitize_wj_inline_tag;
