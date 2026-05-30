afec77e1135973df853558465a4ed45f872bb620
wasd243 <CWwithoutfish@outlook.com>
2026-05-30 17:03:10 -0400

    feat(normalizer): add normalize_include module

--- crates/ltmf/src/preprocess/normalize.rs
+++ crates/ltmf/src/preprocess/normalize.rs
@@ -4,6 +4,7 @@ use crate::preprocess::normalize::{
     normalize_details::normalize_details,
     normalize_footnote::normalize_footnote,
     normalize_hard_break::normalize_hard_break,
+    normalize_include::normalize_include,
     normalize_note::normalize_note,
     normalize_new_line_marks::normalize_new_line_marks,
     normalize_strike::normalize_strike,
@@ -23,6 +24,7 @@ pub mod normalize_white_space_pre_wrap;
 pub mod rename;
 pub mod normalize_tabview;
 pub mod normalize_note;
+pub mod normalize_include;
 // pub mod normalize_div;
 
 pub fn normalize(value: Value) -> Value {
@@ -33,6 +35,7 @@ pub fn normalize(value: Value) -> Value {
     let value = normalize_strike(value);
     let value = normalize_details(value);
     let value = normalize_note(value);
+    let value = normalize_include(value);
     let value = normalize_tabview(value);
     // let value = normalize_div(value);
     normalize_new_line_marks(value)
