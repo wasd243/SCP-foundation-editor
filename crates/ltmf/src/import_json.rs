549524ee733c78d52db811ddad74430d5e0a0171
wasd243 <CWwithoutfish@outlook.com>
2026-05-30 09:37:10 -0400

    test: update test JSON paths

--- crates/ltmf/src/import_json.rs
+++ crates/ltmf/src/import_json.rs
@@ -1,7 +1,7 @@
 // `import_json.rs` is temporarily a test file which hardcoded to `test.json`
 use std::fs;
 
-const JSON_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/src/json/test.json");
+const JSON_PATH: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/test/json/test.json");
 
 pub fn import_json(json: &mut String) {
     *json = fs::read_to_string(JSON_PATH).expect("failed to read test.json");
