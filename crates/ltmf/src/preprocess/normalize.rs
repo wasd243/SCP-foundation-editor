ad26749d5d350080b312c7d71c596e68d9d2c203
wasd243 <CWwithoutfish@outlook.com>
2026-05-30 15:13:09 -0400

    feat(normalizer): add footnote support

--- crates/ltmf/src/preprocess/normalize.rs
+++ crates/ltmf/src/preprocess/normalize.rs
@@ -1,6 +1,7 @@
 use serde_json::Value;
 
 use crate::preprocess::normalize::{
+    normalize_footnote::normalize_footnote,
     normalize_hard_break::normalize_hard_break,
     normalize_strike::normalize_strike,
     normalize_white_space_pre_wrap::normalize_white_space_pre_wrap,
@@ -10,10 +11,12 @@ pub mod normalize_hard_break;
 pub mod normalize_strike;
 pub mod rename;
 pub mod normalize_white_space_pre_wrap;
+pub mod normalize_footnote;
 
 pub fn normalize(value: Value) -> Value {
     // Normalize hard break to NewLine.
     let value = normalize_hard_break(value);
     let value = normalize_white_space_pre_wrap(value);
+    let value = normalize_footnote(value);
     normalize_strike(value)
 }
