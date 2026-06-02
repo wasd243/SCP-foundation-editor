b5cf867de52e2be46e1a5220b0047147cdb58042
wasd243 <wzc_2205@163.com>
2026-06-02 11:53:53 -0400

    feat(interpreter): render tabview components

--- crates/ltmf/src/interpreter/wiki_component.rs
+++ crates/ltmf/src/interpreter/wiki_component.rs
@@ -14,12 +14,14 @@ use crate::interpreter::{
         },
         collapsible::interpret_collapsible,
         footnote::interpret_footnote,
+        tabview::{interpret_tabview, is_tabview},
     },
 };
 pub fn interpret_wiki_component(index: usize, node: &Value) -> Result<String, String> {
     let node_type = expect_node_type(node)?;
     let content = interpret_footnote(node, String::new())?;
     let content = interpret_collapsible(node, content)?;
+    let content = interpret_tabview(node, content)?;
     let content = interpret_align_left(node, content)?;
     let content = interpret_align_center(node, content)?;
     let content = interpret_align_right(node, content)?;
@@ -31,10 +33,10 @@ pub(crate) fn is_wiki_component_node(node: &Value) -> bool {
     is_align_left(node)
         || is_align_right(node)
         || is_align_center(node)
-        || matches!(
-            node_type(node),
-            Some("tabView" | "Collapsible" | "Note" | "Footnote")
-        )
+        || is_tabview(node)
+        // This matches! macro only for identifier
+        // Going to be removed for future implementation
+        || matches!(node_type(node), Some("Collapsible" | "Note" | "Footnote"))
 }
 
 fn expect_node_type(node: &Value) -> Result<&str, String> {
