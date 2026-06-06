644c60b0dd660a24e7e68d03a7af9e9b84254309
wasd243 <CWwithoutfish@outlook.com>
2026-06-06 11:09:31 -0400

    feat(merger): add original source text caching

--- src-tauri/src/handlers/connect_parser.rs
+++ src-tauri/src/handlers/connect_parser.rs
@@ -1,4 +1,17 @@
+use std::fs;
+
+const ORIGIN_SOURCE_TEXT: &str = concat!(
+    env!("CARGO_MANIFEST_DIR"),
+    "/../crates/ltmf/cache/origin.ftml"
+);
+
 #[tauri::command]
 pub fn parse_wikidot(source_text: String) -> Result<w_parser::FtmlParseOutput, String> {
+
+    // cache the original source text to compare with the generated one
+    fs::write(ORIGIN_SOURCE_TEXT, &source_text)
+        .map_err(|err| format!("failed to cache original source text: {err}"))?;
+
+    // let ftml parser parse the source text
     w_parser::render_wikidot_to_html(&source_text)
 }
