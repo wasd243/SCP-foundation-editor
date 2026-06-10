7b1a38c4ae70e26316a87fb0ba1b2c338325d81a
wasd243 <CWwithoutfish@outlook.com>
2026-06-10 10:14:26 -0400

    docs: comments in `src-tauri`

--- src-tauri/src/handlers/connect_exporter.rs
+++ src-tauri/src/handlers/connect_exporter.rs
@@ -1,12 +1,16 @@
 use ltmf::export_wikitext;
 
+/// This function is used to export the ProseMirror JSON to wikitext, based on `ltmf` library.
+///
+/// `ltmf` Directory:
+/// `crates\ltmf\`
 #[tauri::command]
 pub fn export_code(json: String) -> Result<String, String> {
     // println!("{json}");
-
     export_wikitext(&json)
 }
 
+// This function is used to check editor CSS, debug only.
 #[tauri::command]
 pub fn export_css(css: String) {
     println!("{css}");
