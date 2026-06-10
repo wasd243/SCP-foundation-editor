821faf8970b85bb6b38d45709425623a9004275f
wasd243 <CWwithoutfish@outlook.com>
2026-06-10 10:45:03 -0400

    feat(UI): add save button style and positioning adjustments

--- src/components/editor/ToolBar/Actions.vue
+++ src/components/editor/ToolBar/Actions.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import Redo from "./Actions/Redo.vue";
 import Undo from "./Actions/Undo.vue";
+import Save from "./Actions/Save.vue";
 </script>
 
 <template>
   <Redo/>
   <Undo/>
+  <Save/>
 </template>
---
+++ src/components/editor/ToolBar/Actions/Save.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Save from "../../../../assets/icons/Save.svg";
+</script>
+
+<template>
+  <button class="actions-button save" type="button">
+    <img :src="Save" alt="Save"/>
+  </button>
+</template>
