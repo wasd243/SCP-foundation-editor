6f89990e3df38f2e64f77f8a93bb24d0fbb4dd96
wasd243 <CWwithoutfish@outlook.com>
2026-06-11 09:20:34 -0400

    feat(UI): add module css button, update `CREDITS.md`, remove unused `cssModule.vue` in `NonIncludeComponents.vue`, move css button into `InsertCSS.vue`

--- src/components/editor/ToolBar/Insertion.vue
+++ src/components/editor/ToolBar/Insertion.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import InsertImage from "./Insertion/InsertImage.vue";
 import InsertImageBlock from "./Insertion/InsertImageBlock.vue";
+import InsertCSS from "./Insertion/InsertCSS.vue";
 </script>
 
 <template>
   <InsertImage/>
   <InsertImageBlock/>
   <div class="ribbon-separator insertion-normal-1"></div>
+  <InsertCSS/>
 </template>
---
+++ src/components/editor/ToolBar/Insertion/InsertCSS.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import InsertCss from "../../../../assets/icons/InsertCSS.svg";
+</script>
+
+<template>
+  <button class="format-insertion-button css-button" type="button">
+    <img :src="InsertCss" alt="Insert CSS"/>
+    <br>
+    Module CSS
+  </button>
+</template>
