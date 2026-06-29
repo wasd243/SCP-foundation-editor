6f00973c95c2c064535df6476b59c1c5a2cb98a8
wasd243 <CWwithoutfish@outlook.com>
2026-06-29 15:56:41 -0400

    fix: update log_and_write_json to use static file path

--- src-tauri/src/handlers/log_and_write_json.rs
+++ src-tauri/src/handlers/log_and_write_json.rs
@@ -1,8 +1,9 @@
-use crate::utils::path::temp_dir;
 use serde_json::Value;
 use std::fs;
 
+static LOG_FILE_PATH: &str = "dev_output.json";
+
 #[tauri::command]
 pub fn log_and_write_json(json: Value) {
-    fs::write(temp_dir().join("dev_output.json"), json.to_string()).unwrap();
+    fs::write(LOG_FILE_PATH, json.to_string()).unwrap_or_else(|e| panic!("{e}"));
 }
